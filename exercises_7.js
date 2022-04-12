922f49e985a8744c3ce08050dd0a41bee69991de
Gokhan <gokhanarman89mail.com>
2022-04-12 22:53:15 +0300

    exercises7 ve armaganin ilk jssi

--- exercises_7.js
+++ exercises_7.js
@@ -200,5 +200,209 @@ printArray(1,2,3)
 
 // 4. Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
 function showDateTime(){
-    
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth()
+    let year = date.getFullYear()
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+    day < 10 ? day = '0' + day : day = day
+    month < 10 ? month = '0' + month : month = month
+    hour < 10 ? hour = '0' + hour : hour = hour
+    minute < 10 ? minute = '0' + minute : minute = minute
+    console.log(day + '/' + month + '/' + year + ' ' + hour + ':' + minute)
 }
+showDateTime()
+
+// 5. Declare a function name swapValues. This function swaps value of x to y.
+function swapValues(x,y){
+    console.log(x,y)
+    let tempx = x
+    let tempy = y
+    y = tempx
+    x = tempy
+    console.log(x,y)
+}
+swapValues(2,3)
+
+// 6. Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+function reverseArray(...arr){
+    console.log(arr)
+    console.log(arr.length)
+    n = arr.length - 1
+    for(let i = 0;i < n/2;i++){
+        let temp = arr[i]
+        arr[i] = arr[n-i]
+        arr[n-i] = temp
+    }
+    console.log(arr)
+}
+reverseArray(1,2,3,4,5,6,7) // [7, 6, 5, 4, 3, 2, 1]
+
+// 7. Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+function capitalizeArray(...arr){
+    const capitalizedArray = []
+    for(let i = 0;i < arr.length;i++)
+    capitalizedArray.push(arr[i].toUpperCase())
+    return console.log(capitalizedArray)
+}
+capitalizeArray('Gökhan','Hakan','Volkan', 'Ayşenur')
+
+// 8. Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+const array1 = [1,2,3,'GG','Ayşenur']
+const array2 = [4,5,6, 'Keklik', 've']
+function addItem(arr, item){
+    const addedItem = arr
+    addedItem.push(item)
+    return console.log(addedItem)
+}
+addItem(array1, 52) //[1, 2, 3, 'GG', 'Ayşenur', 52]
+addItem(array2, 'Gökhan') //[4, 5, 6, 'Keklik', 've', 'Gökhan']
+
+// 9. Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+function removeItem(arr, index){
+        const removedItem = arr
+        removedItem.splice(index,1)
+        return console.log(removedItem)
+}
+removeItem(array2,3)
+
+// 10. Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
+function sumOfNumbers(number,...arr){
+    let sum = 0
+    for(let i = 0;i < number;i++){
+        sum = sum + arr[i]
+    }
+    return console.log(`${number} times sum of numbers:`,sum)
+}
+sumOfNumbers(4,10,15,20,25,50,60)
+
+// 11. Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that - range.
+function sumOfOdds(number,...arr){
+    sum = 0
+    const oddOfArray = []
+    let tempNumber = number
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] % 2 == 1){
+            oddOfArray.push(arr[i])
+        }
+    }
+    if(number > oddOfArray.length){
+        number = oddOfArray.length
+        console.log(`${tempNumber} parametresi dizinin tek elemanlarindan fazladir.\nO yüzden ${oddOfArray.length} adet numara toplandi.` )
+    }   
+    for(let j = 0;j<number;j++){
+        sum = sum + oddOfArray[j]
+    }
+    return console.log(`${number} times sum of odd numbers:`,sum)
+}
+sumOfOdds(5,10,20,21,30,31,32,34,41)
+
+// 12. Declare a function name sumOfEven. It takes a number parameter and it adds all the even numbers in that - range.
+function sumOfEven(number,...arr){
+    sum = 0
+    const evenOfArray = []
+    let tempNumber = number
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] % 2 == 0){
+            evenOfArray.push(arr[i])
+        }
+    }
+    if(number > evenOfArray.length){
+        number = evenOfArray.length
+        console.log(`${tempNumber} parametresi dizinin çift elemanlarindan fazladir.\nO yüzden ${evenOfArray.length} adet numara toplandi.` )
+    }   
+    for(let j = 0;j<number;j++){
+        sum = sum + evenOfArray[j]
+    }
+    return console.log(`${number} times sum of even numbers:`,sum)
+}
+sumOfEven(8,10,20,21,30,31,32,34,41)
+
+// 13. Declare a function name evensAndOdds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+function evensAndOdds(number,...arr){
+    let countOfEvens = 0
+    let countOfOdds = 0
+    numberItemArray = []
+    evenItemArray = []
+    oddItemArray = []
+    if(number > arr.length){
+        number = arr.length 
+        console.log(`${arr.length} adet dizi elemani vardir.`)      
+    }   
+    for(let i = 0;i < number;i++){
+        numberItemArray.push(arr[i])
+    }
+    for(let j = 0;j < numberItemArray.length;j++){
+        if(numberItemArray[j] % 2 == 0){
+            evenItemArray.push(numberItemArray[j])
+            countOfEvens = evenItemArray.length 
+        }else{
+            oddItemArray.push(numberItemArray[j])
+            countOfOdds = oddItemArray.length
+        }
+    }
+    return console.log(`Count of even number: ${countOfEvens} and Count of odd number: ${countOfOdds}`)
+}
+evensAndOdds(10,10,20,21,30,31,32,34,41)
+
+// 14. Write a function which takes any number of arguments and return the sum of the arguments
+function sumOfArgs(...arr){
+    let sum = 0
+    for(let i = 0;i < arr.length;i++){
+        sum = sum + arr[i]
+    }
+    return console.log(`sum of numbers:`,sum)
+}
+sumOfArgs(4,10,15,20,25,50,60)
+
+// 15. Write a function which generates a randomUserIp.
+function randomUserIp(){  
+    //ip adresses are '192.168.254.255'   
+    let randomNumber1 = Math.floor(Math.random() * 256)
+    let randomNumber2 = Math.floor(Math.random() * 256)
+    let randomNumber3 = Math.floor(Math.random() * 256)
+    let randomNumber4 = Math.floor(Math.random() * 256)
+
+    ipAdress = randomNumber1 + '.' + randomNumber2 + '.' + randomNumber3 + '.' + randomNumber4
+    return console.log('IP adress is:',ipAdress)
+}
+randomUserIp()
+ 
+// 16. Write a function which generates a randomMacAddress
+function randomMacAddress(){
+    let hexDigits = "0123456789ABCDEF";
+    let macAddress = "";
+    for (let i = 0; i < 6; i++) {
+        macAddress += hexDigits.charAt(Math.round(Math.random() * 15));
+        macAddress += hexDigits.charAt(Math.round(Math.random() * 15));
+        if (i != 5) macAddress += ":";
+    }
+
+    return console.log('Mac Adress is:', macAddress);
+}
+randomMacAddress()
+
+// 17. Declare a function name randomHexaNumberGenerator. When this function is called it generates a random hexadecimal number. The function return the hexadecimal number.
+function randomHexaNumberGenerator(){
+    let hexDigits = "0123456789ABCDEF"
+    let randomHexNumber = ""
+    let randomNumber = Math.floor(Math.random() * 100)
+    for (let i = 0;i < randomNumber;i++){
+        randomHexNumber += hexDigits.charAt(Math.floor(Math.random() * 16))
+    } 
+    return console.log("random hexadecimal number:", randomHexNumber)
+}
+randomHexaNumberGenerator()
+
+// 18. Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+function userIdGenerator(){
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let randomsevenchars = ""
+    for(let i = 0; i < 7;i++){
+        randomsevenchars += characters.charAt(Math.floor(Math.random() * characters.length))
+
+    }
+    return console.log(randomsevenchars)
+}
+userIdGenerator()
